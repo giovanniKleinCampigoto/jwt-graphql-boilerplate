959d4499030123b371458f9abce8a5a9a9178af1
giovanniKleinCampigoto <giovannikleincampigoto@gmail.com>
2020-07-24 23:41:57 -0300

    [*] Create backend cookies and protected routes, create refresh token and access token

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,8 +1,18 @@
-import {Resolver, Query, Mutation, Arg} from 'type-graphql'
+import {Resolver, Query, Mutation, Arg, ObjectType, Field, Ctx, UseMiddleware} from 'type-graphql'
 
-import {hash} from 'bcryptjs'
+import {hash, compare} from 'bcryptjs'
 
 import {User} from './entity/User'
+import {MyContext} from './MyContext'
+import {isAuth} from './isAuth'
+
+import {createRefreshToken, createAccessToken} from './auth'
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string
+}
 
 @Resolver()
 export class UserResolver {
@@ -11,10 +21,44 @@ export class UserResolver {
   hello() {
     return 'hi!'
   }
+
+  @Query(() => String)
+  @UseMiddleware(isAuth)
+  bye(
+    @Ctx() {payload}: MyContext
+  ) {
+    console.log(payload?.userId)
+    return `your user id is ${payload!.userId}`
+  }
   
   @Query(() => [User])
   users() {
     return User.find();
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg('email') email: string,
+    @Arg('password') password: string,
+    @Ctx() { res}: MyContext
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: {email}})
+
+    if(!user) {
+      throw new Error('could not find user');      
+    }
+
+    const valid = await compare(password, user.password)
+
+    if(!valid) {
+      throw new Error("bad password")
+    }
+
+    res.cookie('jid', createRefreshToken(user), {httpOnly: true})
+
+    return {
+      accessToken: createAccessToken(user)
+    }
   }
 
   @Mutation(() => Boolean)
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config"
 import "reflect-metadata";
 import express from 'express'
 import {ApolloServer} from 'apollo-server-express'
@@ -18,7 +19,8 @@ import {UserResolver} from './UserResolver'
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [UserResolver]
-        })
+        }),
+        context: ({req, res}) => ({ req,res })
     })
 
     apolloServer.applyMiddleware({app})
